c6c0284c187101f9c2c2c99c59424a20f2d80cf1
maksimka1709 <66752434+maksimka1709@users.noreply.github.com>
2020-07-03 01:51:00 +0300

    Updates

---
+++ precache-manifest.fabd5518c0e31c69076adad9c5d535f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac176e82ab90750072a640cf24111837",
+    "url": "/BlackGtaRp/index.html"
+  },
+  {
+    "revision": "b6cc4d93d546d0353881",
+    "url": "/BlackGtaRp/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "d4ab257035d612365c01",
+    "url": "/BlackGtaRp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b6cc4d93d546d0353881",
+    "url": "/BlackGtaRp/static/js/2.9a45488f.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/BlackGtaRp/static/js/2.9a45488f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ab257035d612365c01",
+    "url": "/BlackGtaRp/static/js/main.9aa61977.chunk.js"
+  },
+  {
+    "revision": "d34542bd58c6e26a0b31",
+    "url": "/BlackGtaRp/static/js/runtime-main.fdb64717.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/BlackGtaRp/static/media/persik.4e1ec840.png"
+  }
+]);
